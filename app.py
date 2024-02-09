14bf6b0e88261df1b6bdde757fb4881d7a31d4e1
Aditya N. Manthri <aditya.manthri@Adityas-MacBook-Pro.local>
2024-02-08 19:12:40 -0500

    minor change

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.wsgi_app = WhiteNoise(app.wsgi_app,
 
 @app.route('/', methods=['GET'])
 def hello():
-    return make_response("Hello, world!!!!!!!!!!!!!!!!")
+    return make_response("add /hw1/ to the url")
     
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
